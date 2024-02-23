aed1575bf31d1d92f7478c11225f9bde6eddef9e
AngPF <angelofonseca68@gmail.com>
2024-02-23 09:32:09 -0300

    .

---
+++ src/main/java/br/com/fiap/moviebox/MovieboxApplication.java
@@ -0,0 +1,21 @@
+package br.com.fiap.moviebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class MovieboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieboxApplication.class, args);
+	}
+
+	@RequestMapping("/home")
+	public String home(){
+		return "Página Inicial! Caralho!";
+	}
+
+}
---
+++ src/test/java/br/com/fiap/moviebox/MovieboxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.moviebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
