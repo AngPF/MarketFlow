df80485a7291f46b46533ef8448d1e05e8b46057
AngPF <angelofonseca68@gmail.com>
2024-03-01 10:13:34 -0300

    .

--- src/main/java/br/com/fiap/moviebox/model/Categoria.java
+++ src/main/java/br/com/fiap/moviebox/model/Categoria.java
@@ -1,7 +1,15 @@
 package br.com.fiap.moviebox.model;
 
+import java.util.Random;
+
 public record Categoria(Long id, String nome) {
 
-    
+    public Categoria(Long id, String nome){
+
+        var key = (id != null) ? id : Math.abs(new Random().nextLong());
+        this.id = key;
+        this.nome = nome;
+
+    }
     
 }
